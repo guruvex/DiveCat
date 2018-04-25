4b3fd86be1e8163d078fd27f3dbc6c25708304da
james heath <guruvex@gmail.com>
2018-04-25 15:25:55 -0600

    forgot to clean the project

--- app/src/main/java/example/android/divecats/MainActivity.java
+++ app/src/main/java/example/android/divecats/MainActivity.java
@@ -7,7 +7,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-     TextView infoText;
+    TextView infoText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,30 +17,41 @@ public class MainActivity extends AppCompatActivity {
         infoText = findViewById(R.id.two);
     }
 
-    /** show open times in the main text view */
+    /**
+     * show open times in the main text view
+     */
     public void showTime(View view) {
         String screenTextPart1 = getResources().getString(R.string.openDaysOne);
         String screenTextPart2 = getResources().getString(R.string.openDaysTwo);
         String screenTextPart3 = getResources().getString(R.string.openDaysThree);
 
-        String screenText = screenTextPart1 + "\n" + screenTextPart2 + "\n" +  screenTextPart3;
+        String screenText = screenTextPart1 + "\n" + screenTextPart2 + "\n" + screenTextPart3;
         infoText.setText(screenText);
     }
-    /** show web site info in the main text view */
+
+    /**
+     * show web site info in the main text view
+     */
     public void showWeb(View view) {
         String screenTextPart1 = getResources().getString(R.string.webSite);
         String screenTextPart2 = getResources().getString(R.string.webSite1);
         String screenText = screenTextPart1 + "\n" + screenTextPart2;
         infoText.setText(screenText);
     }
-    /** show phone number in the main text view */
+
+    /**
+     * show phone number in the main text view
+     */
     public void showPhone(View view) {
         String screenTextPart1 = getResources().getString(R.string.phone);
         String screenTextPart2 = getResources().getString(R.string.phone1);
         String screenText = screenTextPart1 + "\n" + screenTextPart2;
         infoText.setText(screenText);
     }
-    /** show address in the main text view */
+
+    /**
+     * show address in the main text view
+     */
     public void showAddress(View view) {
         String screenTextPart1 = getResources().getString(R.string.address);
         String screenTextPart2 = getResources().getString(R.string.address1);
